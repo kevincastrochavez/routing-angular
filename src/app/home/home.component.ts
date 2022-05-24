9f2bc292faff9ae0cdce739e5f7955eb91d2c0e1
Kevin <kevin.castro.chavez0@gmail.com>
2022-05-23 22:29:51 -0600

    used fake auth service to test login and logout functionality with permissions

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 
+import { AuthService } from "../auth.service";
+
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private authService: AuthService) {}
 
   ngOnInit() {}
 
@@ -16,5 +18,13 @@ export class HomeComponent implements OnInit {
       queryParams: { allowEdit: "1" },
       fragment: "loading",
     });
+  }
+
+  onLogin() {
+    this.authService.login();
+  }
+
+  onLogout() {
+    this.authService.logout();
   }
 }
